9cfae68fd37e5a13899a94a8427f60af4311cecc
Mquanaazi <olwethumalumla31@gmail.com>
2026-07-09 13:27:27 +0200

    added user input error handling and provided funtionality to repeat the game

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -5,8 +5,26 @@
         static void Main(string[] args)
         {
             int maximum = 0;
+            bool GameOver = false;
+            bool validInput = false;
+            string PlayAgain = string.Empty;
+            string difficulty = string.Empty; 
+
+            while (!GameOver)
+
             Console.WriteLine("input a number ");
-            string difficulty = Console.ReadLine();
+            difficulty = Console.ReadLine();
+            while (validInput)
+            
+                if ((difficulty == "E") || (difficulty == "M") || (difficulty == "H"))
+                {
+                    validInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter Y or N.");
+                }
+            
             if (difficulty == "E")
             {
                 maximum = 10;
@@ -15,22 +33,45 @@
             {
                 maximum = 100;
             }
-            else  
+            else if (difficulty == "H")
             {
                 maximum = 1000;
             }
-            NumberGuessing numberGuessing = new NumberGuessing(1 ,maximum);
+            NumberGuessing numberGuessing = new NumberGuessing(1, maximum);
             {
                 Console.WriteLine("Please enter a number between 1 and " + maximum + ":");
                 string guess = Console.ReadLine();
 
             }
-           /* {
-                Console.WriteLine("Invalid input. Please enter E, M, or H.");
-                return;
-            }*/
+            /* {
+                 Console.WriteLine("Invalid input. Please enter E, M, or H.");
+                 return;
+             }*/
+            while (!validInput)
+            { 
+            Console.WriteLine("Do you want to play again ?(Y) or (N)");
+            PlayAgain = Console.ReadLine();
+            PlayAgain = PlayAgain.ToUpper();
+                
+                if (PlayAgain == "Y" || PlayAgain == "N")
+                {
+                    validInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter Y or N.");
+                }
 
-            
+            }
+
+                if (PlayAgain == "Y")
+                {
+                GameOver = false;
+                    }
+                else
+                {
+                GameOver = true;
+                }
         }
     }
 }

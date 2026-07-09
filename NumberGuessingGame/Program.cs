09087234328b1097b0f566ec031de40a7a22cb81
Mquanaazi <olwethumalumla31@gmail.com>
2026-07-09 10:57:09 +0200

    created the user interface

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int maximum = 0;
+            Console.WriteLine("input a number ");
+            string difficulty = Console.ReadLine();
+            if (difficulty == "E")
+            {
+                maximum = 10;
+            }
+            else if (difficulty == "M")
+            {
+                maximum = 100;
+            }
+            else  
+            {
+                maximum = 1000;
+            }
+            NumberGuessing numberGuessing = new NumberGuessing(1 ,maximum);
+            {
+                Console.WriteLine("Please enter a number between 1 and " + maximum + ":");
+                string guess = Console.ReadLine();
+
+            }
+           /* {
+                Console.WriteLine("Invalid input. Please enter E, M, or H.");
+                return;
+            }*/
+
+            
         }
     }
 }
